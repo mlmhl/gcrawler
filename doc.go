8cc8f7909cc7667aa4fc107c0b5d449cfb8d783f
mlmhl <malinbupt@gmail.com>
2018-04-03 18:29:46 +0800

    add doc.go

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package gcrawler provides a very simple and flexible web crawler that supports three running modes:
+
+  Running a fixed period of time
+  Running forever until no more pages to crawled
+  Running forever until explicitly stopped by the `Stop` method
+*/
+package gcrawler
